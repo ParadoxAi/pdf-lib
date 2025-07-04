ce0281b60cb63cd33c4f9f2e2697100ca1d91a60
prd-huy-ta <huy.ta@paradox.ai>
2025-07-04 14:00:01 +0700

    Revert "chore: bump dep"
    This reverts commit 25a7932f776fd2f3e5c4e6a624b16d0f02da3487.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
 
 const { MINIFY, MODULE_TYPE } = process.env;
 
@@ -52,5 +53,5 @@ export default {
     format: MODULE_TYPE,
     sourcemap: true,
   },
-  plugins: [resolve(), commonjs(), json()],
+  plugins: [resolve(), commonjs(), json(), MINIFY === 'true' && terser()],
 };
